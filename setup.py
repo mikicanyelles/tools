f8ae4741f838f39863d7d7d2f3f5116f29a23ce8
mikicanyelles <mcanyellesnino@gmail.com>
2022-11-22 18:50:29 +0100

    First commit including minimal files for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name= 'tools',
+        version='0.0a1',
+        description="Package containing general tools for daily stuff",
+        author="Miquel Canyelles Niño",
+        author_mail="mcanyellesnino@gmail.com",
+        install_requires= ["numpy", "matplotlib"],
+        url= "https://github.com/mikicanyelles/tools",
+        packages= find_packages(),
+        include_package_data=True
+     )
---
+++ tools/__init__.py
@@ -0,0 +1,1 @@
+from tools import plotting_trajectories
